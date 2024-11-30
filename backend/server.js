bc876119f4ff2cbc5cae31baad6be319b9a49976
imad <imadmoujahid26@gmail.com>
2024-11-30 18:33:28 +0100

    Updated backend to bind to dynamic port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
